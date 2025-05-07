8dd356cf5e9a231a21c49c2b26bfbe2f362f2f09
Rona-Gilang <ronagilang29@gmail.com>
2025-05-07 19:48:40 +0700

    formspree error fix4 javascript only the first block code

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ document.getElementById('form-open').addEventListener('click', function() {
   form.style.display = (form.style.display === 'grid') ? 'none' : 'grid';
   // On click check form style display, if grid change to none, else change to grid. 
 });
-document.getElementById('form-offer').addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent the form from refreshing the page.
-  document.getElementById('submit-message').style.display = 'inline'; // Show the message.
-  document.getElementById('form-offer').reset(); // Reset form after submit.
-});
+// document.getElementById('form-offer').addEventListener('submit', function(event) {
+//   event.preventDefault(); // Prevent the form from refreshing the page.
+//   document.getElementById('submit-message').style.display = 'inline'; // Show the message.
+//   document.getElementById('form-offer').reset(); // Reset form after submit.
+// });
 
 // // Toggle form visibility on click.
 // document.getElementById('form-open').addEventListener('click', function() {
